d02807f5d8e64c0accb1aaeb74364763e0db0e0c
Sergey <sukyans@gmail.com>
2020-05-08 21:40:20 -0600

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,12 +23,9 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     @current_cart.line_items.each do |item|
       @order.line_items << item
-      item.cart_id = nil
     end
     @order.save
-    Cart.destroy(session[:cart_id])
-    session[:cart_id] = nil
-    redirect_to root_path
+    redirect_to new_charge_path
   end
 
   private
